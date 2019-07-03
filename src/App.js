643f0505a2ac54d995a2ad33a2c42666a74663f4
DBudak <dmitriybudak@gmail.com>
2019-07-03 11:12:49 -0500

    text input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Text from './text';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    console.log(Text);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/text.js
@@ -0,0 +1,3 @@
+const inputText = 'For over a decade, associations have trusted AffiniPay to handle their credit card transactions. Our knowledgeable and experienced team delivers unmatched service and support to you and your members.';
+
+export default Text = inputText.split(' ');
