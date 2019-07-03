d0560e80fcfdc59951586d967f491c1182cb33d9
DBudak <dmitriybudak@gmail.com>
2019-07-03 11:36:33 -0500

    text output on screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Text from './text';
+import text from './text';
 
 class App extends React.Component {
   componentDidMount() {
     console.log(Text);
   }
   render() {
+    const textOutput = text.map((word, i) => 
+      <span 
+      key={i}>
+        {word+' '} 
+      </span> 
+    );
+
     return (
       <div className="App">
         <header className="App-header">
           <p>
+            {textOutput}
           </p>
         </header>
       </div>
